756a11c3bf35f74dbdc2d7d16030d55eee8e0339
rich <mick7832421@gmail.com>
2022-04-07 00:21:16 +0800

    remove hide

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ DRIVER = os.path.join('.', 'chromedriver.exe')
 
 
 if __name__ == '__main__':
-    #stock_id.update_stock_id(DRIVER, DB_DIR, False)
-    #fundamental.run_crawler(DRIVER, DB_DIR, False)
-    banks_holder.run_crawler(DRIVER, DB_DIR, False)
+    stock_id.update_stock_id(DRIVER, DB_DIR)
+    fundamental.run_crawler(DRIVER, DB_DIR)
+    banks_holder.run_crawler(DRIVER, DB_DIR)
