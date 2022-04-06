e927f911648e0a10c73b1d1291a93362060432c9
rickeyting <mick7832421@gmail.com>
2022-04-06 14:18:49 +0800

    Update main.py
    add fundamental

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils.crawlers import stock_id
+from utils.crawlers import stock_id, fundamental
 import os
 
 DB_DIR = os.path.join('.','raw_data.db')
@@ -6,5 +6,6 @@ DRIVER = os.path.join('.','chromedriver.exe')
 
 
 if __name__ == '__name__':
-  stock_id.update_stock_id(driver, DB_DIR)
+  stock_id.update_stock_id(DRIVER, DB_DIR)
+  fundamental.run_crawler(DRIVER, DB_DIR, False)
   
