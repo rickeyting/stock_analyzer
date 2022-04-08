a3130aadda711922fbdd54377ce22b901551e6b9
rickeyting <mick7832421@gmail.com>
2022-04-08 15:47:56 +0800

    Update postgredb.py
    add postgresql

--- utils/postgredb.py
+++ utils/postgredb.py
@@ -1,1 +1,26 @@
+import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
 
+
+
+def init_tables(engine):
+    init_tables = {
+            'table1': {'cols':['col1','col2'],
+                     'primaries': ['col1']
+                    },
+            }
+            
+    for i in init_tables:
+        table = init_tables[i]
+        df = pd.DataFrame(columns = table['cols'])
+        df.to_sql(table, engine, index=False)
+        query = "ALTER TABLE {} ADD PRIMARY KEY (".format(i)
+        for j in table['primaries']:
+            query += j
+            if table['primaries'].index(j) != len(table['primaries']) -1:
+                query += ", "
+        query += ");"
+        engine.execute(query)
+    
+    return 'tables created'
