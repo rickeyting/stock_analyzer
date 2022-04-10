95153dc61f60822e19115fcc940f94b00326cc01
rich <mick7832421@gmail.com>
2022-04-10 22:18:58 +0800

    techical functions

--- utils/logic/technical.py
+++ utils/logic/technical.py
@@ -3,25 +3,25 @@ import numpy as np
 from utils.postgredb import get_data
 from datetime import datetime, timedelta
 
-
-def get_price_info(days):
-    weekday = datetime.times.weekday()
+def get_price_info(stock_id, days):
+    weekday = datetime.today().weekday()
     if weekday > 5:
         decrease = weekday - 4
     start_data = datetime.strftime(datetime.now() - timedelta(days=decrease + days), '%Y-%m-%d')
-    query = "SELECT * FROM stock_price WHERE date >= '{}'".format(start_data)
+    print(start_data)
+    query = "SELECT * FROM stock_price WHERE date >= '{}' AND stock_id = '{}'".format(start_data, stock_id)
     df = get_data(query)
     return df
 
 
-def TaiwanStockKDJ(days=30):
+def TaiwanStockKDJ(stock_id, days=30):
     '''
     RSV = (close - lowest of days) / (highest of days - lowest of days) * 100
     K = past k * 2 / 3 + RSV * 1 / 3 ,default = 50
     D = past d * 2 / 3 + K * 1 / 3 ,default = 50
     J = ( 3 * d ) - ( 2 * k )
     '''
-    df = get_price_info(days)
+    df = get_price_info(stock_id, days)
     df['MinLow'] = df['min'].rolling(9, min_periods=9).min()
     df['MaxHigh'] = df['max'].rolling(9, min_periods=9).max()
     df['RSV'] = (df['close'] - df['MinLow']) / (df['MaxHigh'] - df['MinLow']) * 100
@@ -32,25 +32,70 @@ def TaiwanStockKDJ(days=30):
     return result
 
 
-def TaiwanStockMACD(days=30):
-    df = get_price_info(days)
-    df['di'] = (df.high-df.low+2*df.close)/4
-    df['ema12'] = df['di'].rolling(12, min_periods=12).mean()
-    df['ema26'] = df['di'].rolling(26, min_periods=26).mean()
-    df['ema12'] = [np.nan] + df['ema12'].tolist()[:-1]
-    print(df)
-    ema12 = []
-    for d,e in df[['di','ema12']].values.tolist():
-        try:
-            ema12.append(e*11/13+d*2/13)
-        except:
-            ema12.append(np.nan)
-    df['ema12'] = ema12
-    print(df)
+def TaiwanStockMACD(stock_id, days=120):
+    df = get_price_info(stock_id, days)
+    df['di'] = (df['max']+df['min']+2*df.close)/4
+    df['ema12'] = df['di'].ewm(span=12, adjust=False).mean()
+    df['ema26'] = df['di'].ewm(span=26, adjust=False).mean()
+    df['dif'] = df['ema12'] - df['ema26']
+    df['MACD'] = df['dif'].ewm(span=9, adjust=False).mean()
+    result = df[['stock_id', 'date', 'ema12', 'ema26', 'dif', 'MACD']]
+    return result
 
 
+def TaiwanStockRSI(stock_id, days=120):
+    df = get_price_info(stock_id, days)
+    df['spread'] = df['close'].diff()
+    df['up'] = df['spread'].clip(lower=0)
+    df['down'] = -1 * df['spread'].clip(upper=0)
+    df['up'] = df['up'].ewm(com=5 - 1, adjust=True, min_periods=5).mean()
+    df['down'] = df['down'].ewm(com=5 - 1, adjust=True, min_periods=5).mean()
+    df['RSI5'] = df['up'] / df['down']
+    df['RSI5'] = 100 - (100/(1+df['RSI5']))
+
+    df['up'] = df['spread'].clip(lower=0)
+    df['down'] = -1 * df['spread'].clip(upper=0)
+    df['up'] = df['up'].ewm(com=10 - 1, adjust=True, min_periods=10).mean()
+    df['down'] = df['down'].ewm(com=10 - 1, adjust=True, min_periods=10).mean()
+    df['RSI10'] = df['up'] / df['down']
+    df['RSI10'] = 100 - (100/(1+df['RSI10']))
+
+    result = df[['stock_id', 'date', 'spread', 'RSI5', 'RSI10']]
+    return result
+
+
+def TaiwanStockBIAS(stock_id, days=120):
+    df = get_price_info(stock_id, days)
+    df['mean'] = df['close'].rolling(10, min_periods=10).mean()
+    df['BIAS10'] = (df['close'] - df['mean']) / df['mean'] * 100
+    df['mean'] = df['close'].rolling(20, min_periods=20).mean()
+    df['BIAS20'] = (df['close'] - df['mean']) / df['mean'] * 100
+    result = df[['stock_id', 'date', 'BIAS10', 'BIAS20']]
+    return result
+
+
+def TaiwanStockWilliams(stock_id, days=120):
+    df = get_price_info(stock_id, days)
+    df['highest'] = df['max'].rolling(9, min_periods=9).max()
+    df['lowest'] = df['min'].rolling(9, min_periods=9).min()
+    df['Williams'] = (df['highest'] - df['close']) / (df['highest'] - df['lowest']) * 100
+    result = df[['stock_id', 'date', 'Williams']]
+    return result
+
+
+#https://investinganswers.com/dictionary/b/bullbear-ratio#:~:text=The%20bull%2Fbear%20ratio%20is%20calculated%20by%20dividing%20the,bullish%20respondents%20by%20the%20number%20of%20bearish%20respondents.?msclkid=7859657fb8d811ecaac9c6e09b27fa05
+def TaiwanStockBBI(stock_id, days=120):
+    df = get_price_info(stock_id, days)
+    df['mean3'] = df['close'].rolling(3, min_periods=3).mean()
+    df['mean6'] = df['close'].rolling(6, min_periods=6).mean()
+    df['mean12'] = df['close'].rolling(12, min_periods=12).mean()
+    df['mean24'] = df['close'].rolling(24, min_periods=24).mean()
+    df['mean72'] = df['close'].rolling(72, min_periods=72).mean()
+    df['BBI24'] = (df['mean3'] + df['mean6'] + df['mean12'] + df['mean24'])/4
+    result = df[['stock_id', 'date', 'BBI24', 'mean3']]
+    return result
 
 
 if __name__ == '__main__':
-    sql = "SELECT * FROM stock_price WHERE date >= '2022-03-01'"
-    get_data(sql)
+    a = TaiwanStockBBI('1101', days=500)
+    print(a)
